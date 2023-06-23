09d945f2b1e12f7277b2b692003c201ed06d5e3c
martina <you@example.com>
2023-06-23 20:26:55 +0100

    Implemented the environmental variables

--- server/db.js
+++ server/db.js
@@ -1,11 +1,15 @@
+// require("dotenv").config();
+
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user: "postgres",
-    password: "123456789",
-    host : "localhost",
-    port: 5432,
-    database: "reviewhero"
+    user: process.env.DBUSER,
+    password:  process.env.DBPASSWORD,
+    host :  process.env.DBHOST,
+    port:  process.env.DBPORT,
+    database:  process.env.DBDATABASE
 });
 
 module.exports = pool;
+
+//Because the app is the single source of thruth, you do not need to keep importing the dotenv config.
